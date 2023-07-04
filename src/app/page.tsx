b20640a7b2390135f989410f2a2537549e671694
dwestland <don@westland.net>
2023-07-04 10:45:06 -0700

    Add seed file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-// import React from 'react'
+import { prisma } from '@/lib/prisma'
 
-const HomePage = () => {
+export default async function Home() {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: 'test@test.com'
+    }
+  })
+
   return (
-    <div>HomePage</div>
+    <main>
+      <div>Hello, {user?.name}</div>
+    </main>
   )
 }
-
-export default HomePage
